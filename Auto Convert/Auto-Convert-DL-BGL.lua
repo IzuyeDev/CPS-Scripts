cf8b1577abe4e4836ea860c178bf4ecf92307459
izuye <84780261+izuye@users.noreply.github.com>
2023-03-24 22:55:07 +0700

    Update Auto-Convert-DL-BGL.lua

--- Auto Convert/Auto-Convert-DL-BGL.lua
+++ Auto Convert/Auto-Convert-DL-BGL.lua
@@ -1,7 +1,7 @@
 delay = 300
-auto_convert_bgl = false --Auto Convert to BGL if there is 100 DLs in Backpack (True = ON, Flase = OFF)
-x =
-y = 
+auto_convert_bgl = true --Auto Convert to BGL if there is 100 DLs in Backpack (True = ON, Flase = OFF)
+x = 0 --Change
+y = 0 --Change
 
 function cekInv(id)
     count = 0
@@ -12,25 +12,23 @@ function cekInv(id)
     end
     return count
 end
-
-xpos = y-1
-ypos = y-1
-
+x = x-1
+y = y-1
 while true do
     if math.floor(cekInv(1796)) >= 100 and auto_convert_bgl == true then
         SendPacket(2,[[action|dialog_return
-        dialog_name|telephone
-        num|53785|
-        x|]]..xpos..[[|
-        y|]]..ypos..[[|
-        buttonClicked|bglconvert]])
+dialog_name|telephone
+num|53785|
+x|]]..x..[[|
+y|]]..y..[[|
+buttonClicked|bglconvert]])
         Sleep(delay)
     end
     SendPacket(2,[[action|dialog_return
-    dialog_name|telephone
-    num|53785|
-    x|]]..xpos..[[|
-    y|]]..ypos..[[|
-    buttonClicked|dlconvert]])
+dialog_name|telephone
+num|53785|
+x|]]..x..[[|
+y|]]..y..[[|
+buttonClicked|dlconvert]])
     Sleep(delay)
 end
